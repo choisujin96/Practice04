f7e016d44fe0b903e97c07836cd7182689e55c51
choisujin96 <csjin9783@naver.com>
2025-04-25 18:35:03 +0900

    6번도 완 캬캬

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -4,10 +4,18 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 		
+		
 		char[] c = new char[]{'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
 		
 		
-			
+		for(int i=0; i<c.length; i++) {
+			if(c[i]==' ') {
+				c[i]=',';
+			}
+			System.out.print(c[i]);
+		}
+		
+			//담는 거 연습하자 뚜딘아...흑흑
 		
 
 		
